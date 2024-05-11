07b373da67eccbc24b1fc93d6ba584f1b513c3a9
Alexandr874 <aleks.ribinsk@mail.com>
2024-05-11 20:17:26 +0300

    адаптив Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,13 +4,14 @@ import photo from '../../../assets/images/foto.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper justifyContent={'space-between'} alignItems={'center'}>
+                <FlexWrapper justifyContent={'space-around'} alignItems={'center'} wrap={'wrap'}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Alexandr Kuvenev</span></Name>
@@ -39,20 +40,29 @@ const SmallText = styled.span`
 `;
 
 const MainTitle = styled.h1`
-    font-size: 27px;
-    font-weight: 400;
+    ${font({
+        weight: 400,
+        Fmin: 20,
+        Fmax: 27
+    })}
+    
 `;
 
 const Name = styled.h2`
-    font-family: Josefin Sans, sans-serif;
-    font-size: 50px;
-    font-weight: 700;
+    ${font({
+        family:"'Josefin Sans', sans-serif",
+        weight: 700,
+        Fmin: 36,
+        Fmax: 50
+    })}
     letter-spacing: 5%;
     margin: 10px 0;
     
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap;
+        
         &::before {
             content: '';
             display: inline-block;
@@ -65,11 +75,16 @@ const Name = styled.h2`
             bottom: 0;
         }
     }
+    
+    @media ${theme.media.mobile} {
+        margin: 15px 0;
+    }
 `;
 
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
     
     &::before {
         content: '';
@@ -82,6 +97,13 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+        
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -17px;
+            left: 20px;
+        }
     }
 `;
 
@@ -89,6 +111,13 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 20px;
+    
+    @media ${theme.media.mobile} {
+        width: 310px;  
+        height: 380px;
+        
+    }
 `;
 
 
